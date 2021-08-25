4cbc9b5efeea982c1e1ba6be60f1b553acfedafb
Matt <Mattxiong_14@hotmail.com>
2021-08-25 09:12:33 -0500

    done with girmiore. still need to finish rolling dice

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,1 +1,52 @@
 // Put your code here
+
+console.log("Do you believe in magic?");
+console.log("------------------------");
+
+const getAllSpells = () => {
+  const spells = [
+    {
+      name: "Turn enemy into a newt",
+      isEvil: true,
+      energyRequired: 5.1,
+    },
+    {
+      name: "Conjure some gold for a local charity",
+      isEvil: false,
+      energyRequired: 2.99,
+    },
+    {
+      name: "Give a deaf person the ability to heal",
+      isEvil: false,
+      energyRequired: 12.2,
+    },
+    {
+      name: "Make yourself emperor of the universe",
+      isEvil: true,
+      energyRequired: 100.0,
+    },
+    {
+      name: "Convince your relatives your political views are correct",
+      isEvil: false,
+      energyRequired: 2921.5,
+    },
+  ];
+
+  return spells;
+};
+
+const makeGoodSpellBook = (allSpells) => {
+  const goodSpellBook = {};
+  goodSpellBook.title = "Good Book";
+  goodSpellBook.spells = allSpells.filter((spell) => spell.isEvil === false);
+
+  return goodSpellBook;
+};
+
+const makeEvilSpellBook = (allSpells) => {
+  const evilSpellBook = {};
+  evilSpellBook.title = "Evil Book";
+  evilSpellBook.spells = allSpells.filter((spell) => spell.isEvil === true);
+
+  return evilSpellBook;
+};
--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,21 @@
 // Put your code here
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+const x = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+for (let i = 0; i < 10; i++) {
+  let die1 = Roll(1, 6);
+  let die2 = Roll(1, 6);
+
+  let message = `${die1.toString()} + ${die2.toString()} == ${die1 + die2}`;
+  if (die1 === die2) {
+    message += " DOUBLES!";
+  } else {
+    console.log(message);
+  }
+}
